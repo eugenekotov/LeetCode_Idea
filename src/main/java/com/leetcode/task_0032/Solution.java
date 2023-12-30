5a4d60ccba39f98d92d1954f1a056ce2c8e81789
eugenekotov <eugene1501@mail.ru>
2023-12-30 23:39:20 +0200

    Task 58

--- src/main/java/com/leetcode/task_0032/Solution.java
+++ src/main/java/com/leetcode/task_0032/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
     public int longestValidParentheses(String s) {
         char[] ch = s.toCharArray();
         int[] zeros = new int[s.length()];
-        List<Integer> stack = new ArrayList<Integer>();
+        List<Integer> stack = new ArrayList<>();
         int maxLength = 0;
         for (int i = 0; i < ch.length; i++) {
             char c = ch[i];
@@ -19,14 +19,14 @@ public class Solution {
                 // )
                 if (!stack.isEmpty()) {
                     int index = stack.remove(stack.size() - 1);
-                    int lenght = i - index + 1;
+                    int length = i - index + 1;
                     // ищем нули и берем длинну
                     if (index > 0) {
-                        lenght = lenght + zeros[index - 1];
+                        length = length + zeros[index - 1];
                     }
-                    zeros[i] = lenght;
-                    if (maxLength < lenght) {
-                        maxLength = lenght;
+                    zeros[i] = length;
+                    if (maxLength < length) {
+                        maxLength = length;
                     }
                 }
             }
